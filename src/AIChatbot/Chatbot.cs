237b195381b5fe35a5056b82da569b7973fc17be
Sushil <ksk.sushil.kumar@gmail.com>
2026-04-18 20:43:14 +0530

    create session to manage chat history

--- src/AIChatbot/Chatbot.cs
+++ src/AIChatbot/Chatbot.cs
@@ -6,47 +6,39 @@ using Microsoft.Extensions.Logging;
 
 namespace AIChatbot;
 
-public sealed class Chatbot(
-    IChatClient chatClient,
-    ILogger<Chatbot> logger
-) 
+public sealed class Chatbot(IChatClient chatClient, ILogger<Chatbot> logger)
 {
+    private readonly ILogger _logger = logger;
+    private readonly AIAgent _chatAgent = chatClient.AsAIAgent(intialSystemPrompt, "chatbot-agent", "A friendly and helpful assistant."
+        //, tools: [
+        //    AIFunctionFactory.Create(ChatbotTools.TellJoke),
+        //    AIFunctionFactory.Create(ChatbotTools.GetWeather)
+        //]
+        );
+
     private const string intialSystemPrompt = "You are a helpful assistant that answers questions.";
 
     public async Task StartChatSession()
     {
-        logger.LogInformation("Starting chat session with the language model {model}", "smollm2:135M");
+        _logger.LogInformation("Starting chat session with the AI model");
 
-        var chatAgent = chatClient.AsAIAgent(intialSystemPrompt, "chatbot-agent", "A friendly and helpful assistant.");
-
-        StringBuilder llmResponseBuffer = new();
-        List<ChatMessage> chatHistory =
-        [
-            new(ChatRole.System, intialSystemPrompt),
-        ];
+        AgentSession session = await _chatAgent.CreateSessionAsync();
 
         string userInput;
-
         while (true)
         {
             Console.Write("You: \n\n");
             userInput = Console.ReadLine() ?? string.Empty;
-            chatHistory.Add(new ChatMessage(ChatRole.User, userInput));
 
             Console.Write("\nChatbot: \n\n");
 
-            IAsyncEnumerable<AgentResponseUpdate> updates = chatAgent.RunStreamingAsync(chatHistory);
+            IAsyncEnumerable<AgentResponseUpdate> updates = _chatAgent.RunStreamingAsync(userInput, session);
 
             await foreach (var update in updates)
             {
                 Console.Write(update.Text);
-
-                llmResponseBuffer.Append(update.Text);
-                llmResponseBuffer.Append(' ');
             }
 
-            chatHistory.Add(new ChatMessage(ChatRole.Assistant, llmResponseBuffer.ToString()));
-            llmResponseBuffer.Clear();
             Console.Write("\n\n");
         }
     }
---
+++ src/AIChatbot/ChatbotTools.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace AIChatbot
+{
+    public class ChatbotTools
+    {
+        [Description("Tells a random joke.")]
+        public static void TellJoke()
+        {
+            List<string> jokes =
+            [
+                "Why don't scientists trust atoms? Because they make up everything!",
+                "Why did the scarecrow win an award? Because he was outstanding in his field!",
+                "Why did the bicycle fall over? Because it was two-tired!",
+                "Why don't skeletons fight each other? They don't have the guts!",
+                "Why did the math book look sad? Because it had too many problems!",
+                "Why did the tomato turn red? Because it saw the salad dressing!",
+                "Why did the coffee file a police report? It got mugged!",
+                "Why does the Java developer wear glasses? Because they don't C#!",
+            ];
+            Random random = new();
+            int index = random.Next(jokes.Count);
+            Console.WriteLine(jokes[index]);
+        }
+
+        [Description("Get the weather for a given location.")]
+        public static string GetWeather([Description("The location to get the weather for.")] string location)
+            => $"The weather in {location} is cloudy with a high of 15°C.";
+    }
+}
